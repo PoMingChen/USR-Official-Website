0f260b3c4590532280aa3065dc6c11da4f2e7aa5
陳柏銘 <s410373092@gm.ntpu.edu.tw>
2018-06-10 17:22:29 +0800

    新增可隱藏或展開之側邊欄（可記錄未來活動，過去熱門文章）等等

---
+++ sidebar_with_hiding/js/index.js
@@ -0,0 +1,60 @@
+ $('.toggle-right-sidebar span').on('click', function() {
+
+   if ($("#right-sidebar").hasClass("hide-right-bar-notifications")) {
+
+     $(".btn-bottom-right-sidebar-close").fadeIn("fast"); //show the button close for sidebar notifications
+
+     //insert class to left sidebar
+     if (!$("#sidebar").hasClass('hide-left-bar')) {
+
+       $("#sidebar").addClass("hide-left-bar");
+       $("#main-content").addClass("merge-left");
+
+       if ($("html").outerWidth() <= 750) {
+         if ($('#sidebar').hasClass('show-left-bar-mobile')) {
+           $('#sidebar').removeClass("show-left-bar-mobile");
+         }
+       }
+
+     } else {
+       if ($("html").outerWidth() <= 750) {
+         if ($('#sidebar').hasClass('show-left-bar-mobile')) {
+           $('#sidebar').removeClass("show-left-bar-mobile");
+         }
+       }
+     }
+
+     $("#right-sidebar").removeClass("hide-right-bar-notifications");
+
+     $("#right-sidebar").addClass(" animated fadeInRight").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+       $(this).removeClass(" animated fadeInRight");
+     });
+
+     $("#main-content").addClass("merge-right-sidebar-notifications");
+
+     $(this).removeClass("fa-outdent");
+     $(this).addClass("fa-indent");
+
+   } else {
+
+     $(".btn-bottom-right-sidebar-close").fadeOut("fast"); //hide the button close for sidebar notifications
+
+     //remove class to left sidebar
+     if ($("#sidebar").hasClass('hide-left-bar')) {
+       $("#sidebar").removeClass("hide-left-bar");
+       $("#main-content").removeClass("merge-left");
+     }
+
+     $("#right-sidebar").addClass("hide-right-bar-notifications");
+
+     $("#right-sidebar").addClass(" animated fadeOutRight").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+       $(this).removeClass(" animated fadeOutRight");
+     });
+
+     $("#main-content").removeClass("merge-right-sidebar-notifications");
+
+     $(this).removeClass("fa-indent");
+     $(this).addClass("fa-outdent");
+   }
+
+ });
